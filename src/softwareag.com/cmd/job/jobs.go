cd4c1f425304b303093ecbe41241d13ab701a4d1
Thorsten A. Knieling <thorsten.knieling@softwareag.com>
2018-12-03 11:26:46 +0100

    Add support for job control and use RFC3339 for date time format provided by swagger

--- src/softwareag.com/cmd/job/jobs.go
+++ src/softwareag.com/cmd/job/jobs.go
@@ -20,22 +20,30 @@
 package job
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"strings"
-
-	"softwareag.com/client/scheduler"
 
 	"github.com/go-openapi/runtime"
 	"softwareag.com/client"
+	"softwareag.com/client/scheduler"
+	"softwareag.com/models"
 )
 
 // List list the jobs
-func List(clientInstance *client.AdabasAdmin, auth runtime.ClientAuthInfoWriter) {
+func List(clientInstance *client.AdabasAdmin, auth runtime.ClientAuthInfoWriter) error {
 	params := scheduler.NewGetJobsParams()
 	resp, err := clientInstance.Scheduler.GetJobs(params, auth)
 	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		switch err.(type) {
+		case *scheduler.GetJobsNotFound:
+			response := err.(*scheduler.GetJobsNotFound)
+			fmt.Println(response.Error())
+		default:
+			fmt.Println("Error:", err)
+		}
+		return err
 	}
 	fmt.Println()
 	fmt.Printf("Name             User        Status     Description\n")
@@ -47,38 +55,118 @@ func List(clientInstance *client.AdabasAdmin, auth runtime.ClientAuthInfoWriter)
 		}
 	}
 	fmt.Println()
+	return nil
 }
 
 // Start the job
-func Start(clientInstance *client.AdabasAdmin, param string, auth runtime.ClientAuthInfoWriter) {
+func Start(clientInstance *client.AdabasAdmin, param string, auth runtime.ClientAuthInfoWriter) error {
 	params := scheduler.NewScheduleJobParams()
 	params.JobName = param
 	resp, err := clientInstance.Scheduler.ScheduleJob(params, auth)
 	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		switch err.(type) {
+		case *scheduler.ScheduleJobBadRequest:
+			response := err.(*scheduler.ScheduleJobBadRequest)
+			fmt.Println(response.Payload.Error.Code, ":", response.Payload.Error.Message)
+		default:
+			fmt.Println("Error:", err)
+		}
+		return err
 	}
-	fmt.Println()
 	fmt.Printf("Status message    : %s\n", resp.Payload.Status.Message)
 	fmt.Printf("Job Name          : %s\n", resp.Payload.Status.Name)
 	fmt.Printf("Execution ID      : %d\n", resp.Payload.Status.ExecutionID)
 	fmt.Println()
+	return nil
+}
+
+// Delete the job
+func Delete(clientInstance *client.AdabasAdmin, param string, auth runtime.ClientAuthInfoWriter) error {
+	params := scheduler.NewDeleteJobParams()
+	params.JobName = param
+	resp, err := clientInstance.Scheduler.DeleteJob(params, auth)
+	if err != nil {
+		switch err.(type) {
+		case *scheduler.DeleteJobBadRequest:
+			response := err.(*scheduler.DeleteJobBadRequest)
+			fmt.Println(response.Payload.Error.Code, ":", response.Payload.Error.Message)
+		default:
+			fmt.Println("Error:", err)
+		}
+		return err
+	}
+	fmt.Println()
+	fmt.Printf("Status message    : %s\n", resp.Payload.Status.Message)
+	fmt.Println()
+	return nil
+}
+
+// DeleteExecution Delete the execution log of a job
+func DeleteExecution(clientInstance *client.AdabasAdmin, param string, auth runtime.ClientAuthInfoWriter) error {
+	params := scheduler.NewDeleteJobResultParams()
+	p := strings.Split(param, ":")
+	params.JobName = p[0]
+	params.JobID = p[1]
+	resp, err := clientInstance.Scheduler.DeleteJobResult(params, auth)
+	if err != nil {
+		switch err.(type) {
+		case *scheduler.DeleteJobResultBadRequest:
+			response := err.(*scheduler.DeleteJobResultBadRequest)
+			fmt.Println(response.Payload.Error.Code, ":", response.Payload.Error.Message)
+		default:
+			fmt.Println("Error:", err)
+		}
+		return err
+	}
+	fmt.Println()
+	fmt.Printf("Status message    : %s\n", resp.Payload.Status.Message)
+	fmt.Println()
+	return nil
+}
+
+// Create nre job using job definition file
+func Create(clientInstance *client.AdabasAdmin, input string, auth runtime.ClientAuthInfoWriter) error {
+	params := scheduler.NewPostJobParams()
+	raw, err := ioutil.ReadFile(input)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	job := &models.JobParameter{}
+	if err := json.Unmarshal(raw, job); err != nil {
+		return err
+	}
+	params.Job = job
+	resp, err := clientInstance.Scheduler.PostJob(params, auth)
+	if err != nil {
+		switch err.(type) {
+		case *scheduler.PostJobBadRequest:
+			response := err.(*scheduler.PostJobBadRequest)
+			fmt.Println(response.Payload.Error.Code, ":", response.Payload.Error.Message)
+		default:
+			fmt.Println("Error:", err)
+		}
+		return err
+	}
+	fmt.Println(resp.Payload.Status.Message)
+	return nil
 }
 
 // Log output
-func Log(clientInstance *client.AdabasAdmin, param string, auth runtime.ClientAuthInfoWriter) {
+func Log(clientInstance *client.AdabasAdmin, param string, auth runtime.ClientAuthInfoWriter) error {
 	params := scheduler.NewGetJobResultParams()
 	v := strings.Split(param, ":")
 	params.JobName = v[0]
 	if len(v) != 2 {
 		fmt.Printf("Parameter should be of form: <job name>:<execution id>\n")
-		return
+		return fmt.Errorf("Parameter should be of form: <job name>:<execution id>")
 	}
 	params.JobID = v[1]
 	resp, err := clientInstance.Scheduler.GetJobResult(params, auth)
 	if err != nil {
 		fmt.Println("Error:", err)
-		return
+		return err
 	}
 	fmt.Println()
 	fmt.Printf("JOB name     : %s\n", resp.Payload.JobResult.Name)
@@ -87,4 +175,5 @@ func Log(clientInstance *client.AdabasAdmin, param string, auth runtime.ClientAu
 	fmt.Printf("JOB ended    : %s\n", resp.Payload.JobResult.Ended)
 	fmt.Printf("Output started -------:\n %s\nOutput ended -------\n", resp.Payload.JobResult.Log)
 	fmt.Println()
+	return nil
 }
